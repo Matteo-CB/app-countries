766da091417e08dc11b9ec4bfe77801e2fedc354
Matteo Chanté-Biyikli <130081405+Matteo-CB@users.noreply.github.com>
2023-07-25 11:47:11 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Footer = () => {
+  return (
+    <footer>
+      <p>Challenge by <a href='https://www.frontendmentor.io/profile/Matteo-CB'>Frontend Mentor</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
